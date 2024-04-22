c81df9720acb6ce0b565c5f8008469be2430a0d1
Kul Karki <Kkarki349@rku.ac.in>
2024-04-22 17:48:21 +0530

    second

---
+++ moterbiekapp/lib/widgets/bikehomepage.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BikeHomePage extends StatefulWidget {
+  const BikeHomePage({super.key});
+
+  @override
+  State<BikeHomePage> createState() => _BikeHomePageState();
+}
+
+class _BikeHomePageState extends State<BikeHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bikes"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: Text(
+                "Categories",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Card(
+                  child: Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4.0),
+                          color: Colors.indigo),
+                      child: Swiper(
+                          itemBuilder: (context, index) {
+                            return Row(
+                              children: [
+                                const SizedBox(width: 20.0),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const [
+                                      Text(
+                                        "Buy, sell, Exchange ",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20.0),
+                                      ),
+                                      Text(
+                                        "All in one place ",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 18.0),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(width: 10.0),
+                                CircleAvatar(
+                                  backgroundColor: Colors.indigo.shade800,
+                                  radius: 50.0,
+                                  child: const Icon(
+                                    FontAwesomeIcons.moneyBill,
+                                    size: 30,
+                                  ),
+                                ),
+                                const SizedBox(width: 20.0)
+                              ],
+                            );
+                          },
+                          itemCount: 2)),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
