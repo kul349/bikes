d9d49f17b8c1e9982f0b8ffc3d970e66c319a1ab
Kul Karki <Kkarki349@rku.ac.in>
2024-04-22 19:25:43 +0530

    adding categories

--- moterbiekapp/lib/widgets/bikehomepage.dart
+++ moterbiekapp/lib/widgets/bikehomepage.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -26,6 +30,11 @@ class _BikeHomePageState extends State<BikeHomePage> {
                 "Categories",
                 style: Theme.of(context).textTheme.titleMedium,
               ),
+            ),
+            CategoryChooser(
+              onTop: (Category) => Navigator.pushNamed(
+                  context, "category_bikes",
+                  arguments: Category),
             ),
             const SizedBox(height: 10.0),
             Padding(
@@ -94,6 +103,14 @@ class _BikeHomePageState extends State<BikeHomePage> {
                     "Recent Posts",
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
+                  const Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "View All",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -102,5 +119,62 @@ class _BikeHomePageState extends State<BikeHomePage> {
         ),
       ),
     );
+  }
+}
+
+class CategoryChooser extends StatelessWidget {
+  final Function(String)? onTop;
+  const CategoryChooser({
+    Key? key,
+    this.onTop,
+  }) : super(key: key);
+  final List<String> types = const [
+    "standard",
+    "Cruiser",
+    "Sport",
+    "Dirt",
+    "Scooter",
+    "Electric"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 100,
+        child: ListView.separated(
+            padding:
+                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              String type = types[index];
+              return GestureDetector(
+                onTap: onTop != null ? () => onTop!(type) : null,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 60.0,
+                      height: 60.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://imgd.aeplcdn.com/1056x594/n/cw/ec/53743/42-right-front-three-quarter-3.jpeg?isig=0&q=80&wm=3"),
+                            fit: BoxFit.cover),
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5.0,
+                    ),
+                    Text(type,
+                        style: const TextStyle(fontWeight: FontWeight.w500))
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const SizedBox(
+                  width: 10.0,
+                ),
+            itemCount: types.length));
   }
 }
