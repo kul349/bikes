28934369d0da7ba81353f79410df7795981120af
Kul Karki <Kkarki349@rku.ac.in>
2024-04-23 22:24:54 +0530

    some changes in bike

--- moterbiekapp/lib/widgets/bikehomepage.dart
+++ moterbiekapp/lib/widgets/bikehomepage.dart
@@ -1,8 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -94,7 +90,6 @@ class _BikeHomePageState extends State<BikeHomePage> {
                 ),
               ),
             ),
-            const SizedBox(height: 16.0),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Row(
